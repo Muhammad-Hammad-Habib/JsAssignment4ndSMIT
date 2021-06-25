a51149d310243212e885e1188b92dd3781e1b21e
Muhammad-Hammad-Habib <Muhammadhammad0315@gmail.com>
2021-06-24 19:11:10 -0700

    Assignment

---
+++ app.js
@@ -0,0 +1,38 @@
+// // Assignment # 43-48
+// // Assignment 4
+
+// // Q1
+// // In HTML file.
+
+// // Q2
+// // In HTML file.
+
+
+// // Q3
+
+// function Del(e){
+//     e.parentNode.parentNode.remove()
+// }
+
+// // Q4
+
+// function image(e){
+//     e.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2sIJi5BbgJXlJhZtUIJgFSmefjdkWaZmEmM3WA_66COLzyjwp5eWsUmrRY4fClkI4vos&usqp=CAU"
+// }
+// function image2(e){
+//     e.src = "https://www.loudegg.com/wp-content/uploads/2020/10/Mickey-Mouse.jpg"
+// }
+
+// // Q5
+
+// var conut = 0 
+
+// function inc(){
+//     conut++;
+//     document.getElementById("num").innerHTML = conut 
+// }
+
+// function dec(){
+//     conut--;
+//     document.getElementById("num").innerHTML = conut 
+// }
